5d1ed1e1c3bd9a64bb7d4058b41f67fe2da49834
Ernest Buffington <ernest.buffington@gmail.com>
2022-11-09 01:19:20 -0500

    Initial Commit

---
+++ includes/css/cms_css.php
@@ -0,0 +1,64 @@
+<?php
+#---------------------------------------------------------------------------------------#
+# HEADER FLYKIT CSS                                                                     #
+#---------------------------------------------------------------------------------------#
+echo "\n\n/* includes/css/cms_css.php Fly Kit for PHP-Nuke Titanium - Design Themes On The Fly */\n"; 
+echo "/* When we are done we will move this code to style.css */\n\n"; 
+?>
+/*---------------------------------------------------------------*/
+/* Global CMS CSS                                                */
+/*                                                               */
+/* Designed and Coded By: Ernest Buffington aka TheGhost         */
+/* Coded On: 16th October, 2022                                  */
+/* Copyright © 2022 Brandon Maintenance Management               */
+/*                                                               */
+/* PLEASE STEAL AND/OR USE THIS CSS CODE                         */
+/* NO NEED FOR WRITTEN PERMISSION                                */
+/* I did not trade a goat for this code!                         */
+/*---------------------------------------------------------------*/
+
+/*---------------------------------------------------------------*/
+/* Global CMS CSS                                                */
+/*---------------------------------------------------------------*/
+.gensmall-visitorlog{
+	font-size: 11px;
+}
+
+font.gensmall-visitorlog
+{
+  font-size: 11px;
+}
+
+img.rounded-corners-shout { 
+  border-radius: 3px;
+}
+
+img.rounded-corners-profile {
+  border-radius: 20px; 
+}
+
+img.rounded-corners-header { 
+  border-radius: 3px; 
+}
+
+img.rounded-corners-last-post { 
+  border-radius: 9px; 
+}
+
+img.rounded-corners-forum {
+  border-radius: 20px; 
+}
+
+img.rounded-corners {
+  border-radius: 20px; 
+}
+
+img.rounded-corners-last-vistors {
+  border-radius: 9px; 
+}
+
+img.rounded-corners-user-info {
+  border-radius: 20px; 
+}
+<?
+
